aa59065d17f5d65143937f2f052ca10a33057d76
Satadeep Dasgupta <satadeep.dasgupta@brainiuminfotech.com>
2026-03-13 13:30:32 +0530

    feat: add MIME type handling for AIME packs in post-install script

--- src-tauri/resources/post-install.sh
+++ src-tauri/resources/post-install.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+mkdir -p /usr/share/mime/packages
+cat > /usr/share/mime/packages/aimepack.xml << 'EOF'
+<?xml version="1.0" encoding="UTF-8"?>
+<mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+  <mime-type type="application/x-aimepack">
+    <comment>AIME Lesson Pack</comment>
+    <glob pattern="*.aimepack"/>
+    <magic priority="60">
+      <match type="string" value="PK" offset="0"/>
+    </magic>
+    <sub-class-of type="application/zip"/>
+  </mime-type>
+</mime-info>
+EOF
 update-mime-database /usr/share/mime || true
 update-desktop-database || true
 gtk-update-icon-cache -f -t /usr/share/icons/hicolor || true
